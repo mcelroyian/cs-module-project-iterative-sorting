4b918d3dcd098fefd4048b7a0148358a2d5650eb
Ian McElroy <mcelroyian@gmail.com>
2020-07-21 14:44:33 -0500

    final

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,15 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
-
+    start = 0
+    end = len(arr) -1
+    
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return mid
+        if target < arr[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
     return -1  # not found
